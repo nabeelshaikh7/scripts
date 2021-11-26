a6989a3963e813ee6b331b8a9e75fe3e9b4c5059
Nabeel shaikh <72862916+nabeelshaikh7@users.noreply.github.com>
2021-11-26 15:34:59 +0530

    just added -y

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,8 +1,8 @@
 #updating the apt repo
-sudo apt update
+sudo apt update -y
 
 #installing dependencies
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 
 #downloading and adding the docker gpg key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -11,10 +11,10 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 
 #updating the apt repo
-sudo apt update
+sudo apt update -y
 
 #installing docker-ce
-sudo apt install docker-ce
+sudo apt install -y docker-ce
 
 #to avoide typing sudo command this command 
 sudo usermod -aG docker ${USER}
