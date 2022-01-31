414ced0b1b26622c0097b72c8b602e2a9e4a6cfe
Nabeel shaikh <72862916+nabeelshaikh7@users.noreply.github.com>
2022-01-31 13:43:34 +0530

    added official docker repos

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,20 +1,22 @@
 #updating the apt repo
 sudo apt update -y
+#uninstalling old versions of docker 
+sudo apt-get remove docker docker-engine docker.io containerd runc
 
-#installing dependencies
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install ca-certificates curl gnupg lsb-release
 
-#downloading and adding the docker gpg key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#adding Docker gpg keys
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-#adding docker repo to apt sources
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+#adding docker apt repo
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#updating apt
+sudo apt-get update
 
-#updating the apt repo
-sudo apt update -y
-
-#installing docker-ce
-sudo apt install -y docker-ce
+#installing docker
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 #install docker-compose
 sudo apt install docker-compose
